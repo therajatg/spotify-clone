cf73f2bb002e7ea49fe781a15341d26d07315466
Rajat Gupta <dguptarajat@gmail.com>
2023-07-24 23:37:26 +0530

    sidebar

---
+++ components/Library.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { TbPlaylist } from "react-icons/tb";
+const Library = () => {
+  return (
+    <div className="flex flex-col">
+      <div className="flex items-center justify-between px-5 pt-4">
+        <div className="inline-flex items-center gap-x-2">
+          <TbPlaylist size={26} className="text-neutral-400" />
+          <p className>Your library</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Library;
